28d3f3bde2221f6ca56de2b863fbf83997398c1a
-m <-mkhalidhissoune962@gmail.com>
2024-03-06 23:49:54 +0100

    something

--- resources/views/layouts/gestnav.blade.php
+++ resources/views/layouts/gestnav.blade.php
@@ -2,7 +2,15 @@
     @if (Route::has('login'))
         <div class=" sm:top-0 sm:right-0 p-6 text-right z-10">
             @auth
-                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @role('Admin')
+            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @endrole
+            @role('owner')
+            <a href="{{ url('/dashboard_oner') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @endrole
+            @role('ownar')
+            <a href="{{ url('/dashboard_oper') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @endrole
             @else
                 <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
